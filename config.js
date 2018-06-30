8715e589dac0ca7066ddfd6544e198538c78eba3
softvar <varun2902@gmail.com>
2018-07-01 02:17:33 +0530

    auto-generated config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+{
+  "name": "test",
+  "desc": "test",
+  "chrome": {
+    "plugins": [
+      {
+        "name": "p1",
+        "version": "1.0.2",
+        "permissions": []
+      },
+      {
+        "name": "p2",
+        "version": "1.0.2",
+        "permissions": []
+      }
+    ]
+  }
+}
